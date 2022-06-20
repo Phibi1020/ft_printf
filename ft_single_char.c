d79f417028c6021cca0d4b3fd88dbfc484bb4f2a
Phibi1020 <foongjing5@gmail.com>
2022-06-20 17:56:01 +0800

    norm

--- ft_single_char.c
+++ ft_single_char.c
@@ -6,7 +6,7 @@
 /*   By: ftan <ftan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 10:56:46 by ftan              #+#    #+#             */
-/*   Updated: 2022/06/20 16:15:59 by ftan             ###   ########.fr       */
+/*   Updated: 2022/06/20 17:52:45 by ftan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ int	ft_single_char(va_list args)
 	c = (char)va_arg(args, int);
 	i = write(1, &c, 1);
 	if (i > 0)
-		return i;
+		return (i);
 	return (0);
 }
--- ft_string.c
+++ ft_string.c
@@ -6,25 +6,25 @@
 /*   By: ftan <ftan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 11:37:08 by ftan              #+#    #+#             */
-/*   Updated: 2022/05/29 13:38:39 by ftan             ###   ########.fr       */
+/*   Updated: 2022/06/20 17:54:48 by ftan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_string(va_list args)
+int ft_string (va_list args)
 {
-    char    *str;
-    int     i;
+	char	*str;
+	int		i;
 
-    i = 0;
-    str = va_arg(args, char *);
-    if (!str)
-        i = write(1, "(null)", 6);
-    else
-    {
-        while (str[i])
-            write(1, &str[i++], 1);
-    }
-    return (i);
+	i = 0;
+	str = va_arg(args, char *);
+	if (!str)
+		i = write(1, "(null)", 6);
+	else
+	{
+		while (str[i])
+			write(1, &str[i++], 1);
+	}
+	return (i);
 }
